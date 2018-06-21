515b3d41b1d4ee73e5619d16d21ae12dbbdbaf2a
themostretarded <marcosresidentevil@hotmail.com>
2018-06-20 21:17:44 -0500

    Introduccion lista
    Vamos por el capitulo 1

--- Juegos/capitulo-1.js
+++ Juegos/capitulo-1.js
@@ -1,8 +1,18 @@
 //test that pixi is working-- console.log(PIXI);
+
 //crer el renderizador
 let renderer = PIXI.autoDetectRenderer(256,256);
+
+/*para forzar canvas drawingapi sobre opengl usa
+let renderer = new PIXI.CanvasRenderer(256,256);
+y esto es para forzar la otra manera
+let renderer = new PIXI.WebGLRenderer(256,256);
+*/
+
 //añadir canvas al documento html
 document.body.appendChild(renderer.view);
+
 //crear el contener un objeto llamado stage(escenario)
 let stage = new PIXI.Container();
+
 renderer.render(stage);
