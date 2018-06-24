7b7b0ab15ea2bc5958de388da0296fced83243d8
themostretarded <marcosresidentevil@hotmail.com>
2018-06-23 22:18:51 -0500

    Avance del capitulo 1
    voy en la pagina 7 donde muestran como hacer uso del texture cache y texture asi como de la clase de pixi viendo en el capitulo los tres modos de usar este

--- Juegos/capitulo-1.js
+++ Juegos/capitulo-1.js
@@ -16,3 +16,15 @@ document.body.appendChild(renderer.view);
 let stage = new PIXI.Container();
 
 renderer.render(stage);
+
+/*
+una imagen en el formato para webgl se le llama textura
+el texture cache almacena y referencia todas las texturas de los sprites
+los nombres de textura son string de la direccion de donde salio
+puedes encontrar tus texturas asi
+PIXI.utils.TexturaCache["image/anyImage.png"];
+de la siguiente manera es el modo en el que podemos convertir una imagen en textura
+para que el pixirenderer pueda ser mas eficiente
+let texture = PIXI.utils.TextureCache["image/anyImage.png"];
+let sprite = new PIXI.Sprite(texture);
+*/
