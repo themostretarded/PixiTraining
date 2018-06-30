d507d5cf50cba4167168f3eff42057e577bc2bac
themostretarded <marcosresidentevil@hotmail.com>
2018-06-29 21:07:12 -0500

    Termine el capitulo 1
    bueno solo relee todo el capitulo 1 y apunta cosas y con eso podemos decir que terminanos

--- public/Juegos/capitulo-1-5.js
+++ public/Juegos/capitulo-1-5.js
@@ -35,6 +35,7 @@ function setup() {
     dungeon = new Sprite(dungeonTexture);
     stage.addChild(dungeon);
 
+
     //2. Access the texture using throuhg the loader's `resources`:
     explorer = new Sprite(resources["image/atlasdungeon.json"].textures["explorer.png"]);
     explorer.x = 68;
@@ -62,6 +63,39 @@ function setup() {
     door.position.set(32, 0);
     stage.addChild(door);
 
-    //Render the stage  
-  renderer.render(stage);
+    //Make the blobs
+    let numberOfBlobs = 6,
+        spacing = 48,
+        xOffset = 150;
+
+    //Make as many blobs as there are `numberOfBlobs`
+    for (let i = 0; i < numberOfBlobs; i++) {
+
+        //Make a blob
+        let blob = new Sprite(id["blob.png"]);
+
+        //Space each blob horizontally according to the `spacing` value.
+        //`xOffset` determina el punto desde la izquierda de la pantalla
+        //en el que debe agregarse el primer blob.
+        let x = spacing * i + xOffset;
+
+        //Give the blob a random y position
+        //(`randomInt` is a custom function - see below)
+        let y = randomInt(0, stage.height - blob.height);
+
+        //Set the blob's position
+        blob.x = x;
+        blob.y = y;
+
+        //Add the blob sprite to the stage
+        stage.addChild(blob);
+    }
+
+    //Render the stage   
+    renderer.render(stage);
+}
+
+//The `randomInt` helper function
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
